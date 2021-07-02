bc2c88525a7455e0921a1000bcff4edd2bb31349
Oladele Joshua <jurshsmith@gmail.com>
2021-07-02 19:02:43 +0100

    wraps up ERC721

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -560,6 +560,11 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
     // TODO: Create private vars for token _name, _symbol, and _baseTokenURI (string)
 
     // TODO: create private mapping of tokenId's to token uri's called '_tokenURIs'
+    string private _name;
+    string private _symbol;
+    string private _baseTokenURI;
+
+    mapping(uint256 => string) private _tokenURIs;
 
     bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
 
@@ -593,6 +598,10 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
     // TIP #2: you can also use uint2str() to convert a uint to a string
     // see https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol for strConcat()
     // require the token exists before setting
+    function setTokenURI(uint256 tokenId) public {
+        require(_exists(tokenId), "ERC721: token does not exist");
+        _tokenURIs[tokenId] = strConcat(_baseTokenURI, uint2str(tokenId));
+    }
 }
 
 //  TODO's: Create CustomERC721Token contract that inherits from the ERC721Metadata contract. You can name this contract as you please
@@ -603,3 +612,20 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 //      -takes in a 'to' address, tokenId, and tokenURI as parameters
 //      -returns a true boolean upon completion of the function
 //      -calls the superclass mint and setTokenURI functions
+contract DecentralizedHousingToken is ERC721Metadata {
+    string private _tokenName = "IDecentralizedHousingToken";
+    string private _symbolName = "IDHT";
+    string private _baseTokenURI =
+        "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
+
+    constructor()
+        public
+        ERC721Metadata(_tokenName, _symbolName, _baseTokenURI)
+    {}
+
+    function mint(address to, uint256 tokenId) public onlyOwner returns (bool) {
+        super._mint(to, tokenId); // to access inherit stateful constructor contract function
+        super.setTokenURI(tokenId);
+        return (true);
+    }
+}
