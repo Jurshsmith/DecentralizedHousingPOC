76849af1c6d81d9529ac347ae6d20bcad2475282
Oladele Joshua <jurshsmith@gmail.com>
2021-07-02 09:19:46 +0100

    updates the task 1 for ERC 721 mintable

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -8,16 +8,20 @@ import "./Oraclize.sol";
 
 contract Ownable {
     //  TODO's
-    //  1) create a private '_owner' variable of type address with a public getter function
     //  2) create an internal constructor that sets the _owner var to the creater of the contract 
     //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
     //  4) fill out the transferOwnership function
     //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+    address private _owner;
 
     function transferOwnership(address newOwner) public onlyOwner {
         // TODO add functionality to transfer control of the contract to a newOwner.
         // make sure the new owner is a real address
 
+    }
+
+    function getOwner() public view returns(address){
+        return _owner;
     }
 }
 
